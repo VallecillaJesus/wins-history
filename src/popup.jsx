ffe8e21a966e64af09b24f8a114a5987f4166ea7
VallecillaJesus <jesusvallecilla27@gmail.com>
2022-01-27 17:04:30 -0500

    Gracias mundo

--- src/popup.jsx
+++ src/popup.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {render} from 'react-dom'
 
 const Popup = () => {
-  return <div>"Daniel Gomez Piadiache like"</div>;
+  return <div>The fight is on the way ok</div>;
 };
 
 render(<Popup/>,document.getElementById("app"))
